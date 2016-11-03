ba044a098611766d0d1555bc1cd249f598e7b47c
nel215 <otomo.yuhei@gmail.com>
2016-11-03 19:07:52 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+from setuptools import setup
+
+
+setup(
+    name='ksvd',
+    version='0.0.1',
+    description='An K-SVD implementaion written in Python.',
+    author='nel215',
+    author_email='otomo.yuhei@gmail.com',
+    url='https://github.com/nel215/ksvd',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Scientific/Engineering :: Mathematics',
+    ],
+    packages=['ksvd'],
+    install_requires=[
+        "numpy",
+        "scikit-learn",
+    ],
+    keywords=['machine learning'],
+)
