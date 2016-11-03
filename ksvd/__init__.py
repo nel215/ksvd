8f2489ad224432d45d349f064d784d074f1cf435
nel215 <otomo.yuhei@gmail.com>
2016-11-03 12:39:15 +0900

    Separate update dictionary function

--- ksvd/__init__.py
+++ ksvd/__init__.py
@@ -10,6 +10,21 @@ class ApproximateKSVD(object):
         self.tol = 1e-6
         self.n_components = n_components
 
+    def _update_dict(self, X, D, gamma):
+        for j in range(self.n_components):
+            D[j, :] = 0
+            I = gamma[:, j] > 0
+            if np.sum(I) == 0:
+                continue
+
+            g = gamma[I, j].T
+            d = X[I, :].T.dot(g) - gamma[I, :].dot(D).T.dot(g)
+            d /= np.linalg.norm(d)
+            g = X[I, :].dot(d) - gamma[I, :].dot(D).dot(d)
+            D[j, :] = d
+            gamma[I, j] = g.T
+        return D, gamma
+
     def fit(self, X):
         D = np.random.randn(self.n_components, X.shape[1])
         for i in range(self.max_iter):
@@ -19,19 +34,8 @@ class ApproximateKSVD(object):
             e = np.linalg.norm(X - gamma.dot(D))
             if e < self.tol:
                 break
-            for j in range(self.n_components):
-                D[j, :] = 0
-                I = gamma[:, j] > 0
-                if np.sum(I) == 0:
-                    continue
+            D, gamma = self._update_dict(X, D, gamma)
 
-                g = gamma[I, j].T
-                d = X[I, :].T.dot(g) - gamma[I, :].dot(D).T.dot(g)
-                d /= np.linalg.norm(d)
-                g = X[I, :].dot(d) - gamma[I, :].dot(D).dot(d)
-                D[j, :] = d
-                gamma[I, j] = g.T
-                e = np.linalg.norm(X - gamma.dot(D))
         self.components_ = D
 
     def transform(self, X):
